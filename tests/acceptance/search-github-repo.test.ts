0d3303f1ebdcb32153e778632ab0572f02c2ab49
ThankGod Ossaija (Tg) <codebugsolved@gmail.com>
2022-03-02 11:16:56 +0100

    test github search api

--- tests/acceptance/search-github-repo.test.ts
+++ tests/acceptance/search-github-repo.test.ts
@@ -1,7 +1,24 @@
 import { Api } from './utils/api'
 
 describe('Given that we have a healthy service', () => {
-  test('hdhd', () => {
-    expect(2).toBe(2)
+  test('Can get github repo with mongodb connection db', async() => {
+    const response = await Api.get('/v1/search').expect(200)
+    expect(response.body).toHaveProperty('items')
+    expect(response.body).toHaveProperty('total_count')
+    expect(response.body.items.length).toBe(30)
+  })
+
+  test('Can get github repo with mongodb connection db given a pageSize', async() => {
+    const response = await Api.get('/v1/search?pageSize=2').expect(200)
+    expect(response.body).toHaveProperty('items')
+    expect(response.body).toHaveProperty('total_count')
+    expect(response.body.items.length).toBe(2)
+  })
+
+  test('Can get github repo with mongodb connection db given a searchString', async() => {
+    const response = await Api.get('/v1/search?searchString=tg').expect(200)
+    expect(response.body).toHaveProperty('items')
+    expect(response.body).toHaveProperty('total_count')
+    expect(response.body.items[0].name).toMatch(/tg/i)
   })
 })
